30d023afcf1454892eeafb25d350014f3fe2e4c8
Mohsina Rahman Mirza <142750535+Mohsina19@users.noreply.github.com>
2025-08-14 23:30:35 +0600

    Modify Register Page #7

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import { Link, useNavigate } from "react-router-dom";
+=======
+import { Link } from "react-router-dom";
+>>>>>>> 4299ea9 (register.jsx)
 
 export default function Login() {
   const navigate = useNavigate();
@@ -8,6 +12,7 @@ export default function Login() {
   };
 
   return (
+<<<<<<< HEAD
     <div className="flex justify-center items-center min-h-[calc(100vh-64px)] bg-gradient-to-r from-blue-100 to-purple-100">
       <div className="w-full max-w-xs bg-white rounded-lg shadow-md p-4">
         <div className="text-center mb-2">
@@ -15,6 +20,15 @@ export default function Login() {
           <p className="text-gray-500 mt-1 text-xs">
             Welcome back! Please login to your account
           </p>
+=======
+    <div className="flex justify-center items-center min-h-screen bg-gray-50">
+      <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
+        
+        {/* Logo / Brand */}
+        <div className="text-center mb-4">
+          <h1 className="text-blue-600 font-bold text-lg cursor-pointer">FitMind</h1>
+          <p className="text-blue-500 text-sm cursor-pointer">Login</p>
+>>>>>>> 4299ea9 (register.jsx)
         </div>
 
         <form>
@@ -61,10 +75,14 @@ export default function Login() {
 
         <p className="mt-3 text-center text-gray-600 text-xs">
           Not a member?{" "}
+<<<<<<< HEAD
           <Link
             to="/register"
             className="text-blue-500 font-medium hover:underline"
           >
+=======
+          <Link to="/register" className="text-blue-500 hover:underline">
+>>>>>>> 4299ea9 (register.jsx)
             Signup now
           </Link>
         </p>
